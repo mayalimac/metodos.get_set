ca293dcea8a5f35d52d9e539c4b48368d4fa6f21
Mayara Lima <91838870+mayalimac@users.noreply.github.com>
2022-08-24 10:00:53 -0300

    Add files via upload

---
+++ get_set1.py
@@ -0,0 +1,25 @@
+class Funcionario:
+
+def__init__ (self,nome,sobrenome,idade,endereço):
+self.__nome= nome
+self.__sobrenome= sobrenome
+self.__idade= idade
+self.__endereço= endereço
+
+def get_nome(self):
+ return self.__nome
+
+def get_sobrenome(self):
+ return self.__sobrenome
+
+def get_idade(self):
+ return self.__idade
+
+def get_endereço(self):
+ return self.__endereço
+
+def set_nome(self):
+return self.__nome
+
+def__init__(self):
+ return f"Nome:{self.__none}\nSobrenome: {self.__sobrenome}\nIdade:{self.__idade}\nEndereço: {self.__endereço}"
